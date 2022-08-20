e68540ca129dd27f9c3a5cf14ee9f6e6b9ef4bf8
AlexeySkrylnikov <77483136+AlexeySkrylnikov@users.noreply.github.com>
2022-08-20 17:32:30 +0500

    ДЗ по 1 уроку готово

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 'use sctrict'
+
+const renderProduct = product => {
+    return `<div class="product">
+        <h3>${product.title}</h3>
+        <img class="productImg" src="${product.img}">
+        <p class="price">Цена: ${product.price}</p>
+        <button class="buyButton">Купить</button>
+    </div>`
+};
+
+const renderPage = list => {
+    document.querySelector('.productWrapper').innerHTML = list.map(product => renderProduct(product)).join('');
+    document.querySelector('.footerText').innerHTML = 'Все права защищены © ' + new Date().getFullYear();
+};
+
+renderPage(products);
