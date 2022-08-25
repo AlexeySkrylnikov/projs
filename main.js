1bee84aace2c78480c5b8820835713223bcbf0dc
AlexeySkrylnikov <77483136+AlexeySkrylnikov@users.noreply.github.com>
2022-08-25 22:41:27 +0500

    ДЗ по 2 уроку готово

--- main.js
+++ main.js
@@ -1,1 +1,88 @@
-'use sctrict'
+class ProductList {
+    constructor(container = '.productWrapper') {
+        this.container = container;
+        this.goods = [];
+        this._fetchProducts(products);
+        this.render();
+        this.getProductsSum();
+    }
+
+    _fetchProducts(products) {
+        products.forEach(item => this.goods.push(item));
+    }
+
+    /**
+     * Задание 2. Метод для подсчета суммы всех товаров
+     * @total итоговая сумма
+     */
+    getProductsSum() {
+        let total = 0;
+        this.goods.forEach(item => total += item.price);
+        console.log(`Сумма всех товаров ${total}`);
+    }
+
+    render() {
+        const block = document.querySelector(this.container);
+        for (let product of this.goods) {
+            const item = new ProductItem(product);
+            block.insertAdjacentHTML("beforeend", item.render());
+        }
+    }
+}
+
+class ProductItem {
+    constructor(product) {
+        this.title = product.title;
+        this.id = product.id;
+        this.price = product.price;
+        this.img = product.img;
+    }
+
+    render() {
+        return `<div class="product">
+        <h3>${this.title}</h3>
+        <img class="productImg" src="${this.img}">
+        <p class="price">Цена: ${this.price}</p>
+        <button class="buyButton">Купить</button>
+    </div>`
+    }
+}
+
+// Задание 1
+class Basket {
+    constructor(container) {
+        this.container = container;
+    }
+
+    _addToBasket() {
+
+    }
+
+    _removeFromBasket() {
+
+    }
+
+    _getBasketTotalSum() {
+
+    }
+
+    render() {
+
+    }
+}
+
+// Задание 1
+class BasketItem {
+    constructor(item) {
+        this.title = item.title;
+        this.id = item.id;
+        this.price = item.price;
+    }
+
+    render() {
+
+    }
+
+}
+
+let list = new ProductList();
---
+++ products.js
@@ -0,0 +1,50 @@
+const products = [
+    {
+        id: 1,
+        title: "Ноутбук",
+        price: 25000,
+        img: "img/products/nout.jpg"
+    },
+    {
+        id: 2,
+        title: "Клавиатура",
+        price: 1400,
+        img: "img/products/klava.jpg"
+    },
+    {
+        id: 3,
+        title: "Мышка",
+        price: 500,
+        img: "img/products/mouse.jpg"
+    },
+    {
+        id: 4,
+        title: "Роутер",
+        price: 4500,
+        img: "img/products/router.jpg"
+    },
+    {
+        id: 5,
+        title: "Ноутбук 2",
+        price: 25000,
+        img: "img/products/nout.jpg"
+    },
+    {
+        id: 6,
+        title: "Клавиатура 2",
+        price: 1400,
+        img: "img/products/klava.jpg"
+    },
+    {
+        id: 7,
+        title: "Мышка 2",
+        price: 500,
+        img: "img/products/mouse.jpg"
+    },
+    {
+        id: 8,
+        title: "Роутер 2",
+        price: 4500,
+        img: "img/products/router.jpg"
+    }
+]
